36bc2a3786a7e83d0e0618037fca6ef07337be0f
Aditya Kashyap <adityakashyap5047@gmail.com>
2024-07-05 23:43:22 +0530

    changing the message

--- client/src/context/AccountProvider.jsx
+++ client/src/context/AccountProvider.jsx
@@ -12,7 +12,7 @@ const AccountProvider = ({children}) => {
     const socket = useRef();
 
     useEffect(() => {
-        // socket.current = io("ws://localhost:9000");
+        // socket.current = io("ws://localhost:9000");   This is for local host
         socket.current = io("wss://whatstalk-socket.onrender.com:10000");
     }, []);
 
