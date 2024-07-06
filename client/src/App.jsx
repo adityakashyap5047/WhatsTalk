631048c857c42301046b55ccd980dd4e3ac0550d
Aditya Kashyap <adityakashyap5047@gmail.com>
2024-07-06 09:57:22 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,7 @@ import Messenger from "./components/Messenger.jsx";
 import AccountProvider from './context/AccountProvider.jsx';
 
 function App() {
-  const clientId = '984047477247-caaiea1pg79l139kekiu8ce305hbrrg1.apps.googleusercontent.com';
+  const clientId = '******************************************************************************';
   return (
     <GoogleOAuthProvider clientId={clientId}>
       <AccountProvider><Messenger/></AccountProvider>
